cc9ddd8f7fdce919d73061006620d548cbef039b
kc910521 <kc910521@qq.com>
2020-01-27 17:33:33 +0800

    查找

--- src/main/java/ind/ck/leetcode/EzLeetCode.java
+++ src/main/java/ind/ck/leetcode/EzLeetCode.java
@@ -10,8 +10,40 @@ import java.util.Arrays;
 public class EzLeetCode {
 
     public static void main(String[] args) {
-        System.out.println(firstUniqChar(""));
+//        System.out.println(firstUniqChar("loveleetcode"));
+//        System.out.println(0 - 'a');
+//        char c = "我abcd发".charAt(0);
+//        char c1 = "我abcd发".charAt(1);
+        System.out.println(isAnagram("我at", "c我a"));
     }
+
+    /**
+     * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/35/
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public static boolean isAnagram(String s, String t) {
+        if (s == null || t == null) {
+            return false;
+        }
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = t.toCharArray();
+        if (chars1.length != chars2.length) {
+            return false;
+        }
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+        for (int i = 0; i < chars1.length; i++) {
+            if (chars1[i] != chars2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 
 
     /**
@@ -21,12 +53,20 @@ public class EzLeetCode {
      * @return
      */
     public static int firstUniqChar(String s) {
-        int[] b = new int[26];
+        int[] iDict = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            b[s.charAt(i) - 'a']++;
+            int cidx = s.charAt(i) - 'a';
+            if (cidx >= 0 && cidx <= 25) {
+                iDict[cidx]++;
+            }
         }
         for (int i = 0; i < s.length(); i++) {
-            if (b[s.charAt(i) - 'a'] < 2) return i;
+            int cidx = s.charAt(i) - 'a';
+            if (cidx >= 0 && cidx <= 25) {
+                if (iDict[cidx] == 1) {
+                    return i;
+                }
+            }
         }
         return -1;
     }
